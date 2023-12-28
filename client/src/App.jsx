0be26581d1eb4ad12f94e9a749db8029f92da7e4
BukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2023-12-28 18:25:24 +0100

    apis

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,8 +13,10 @@ import PlacePage from "./pages/PlacePage";
 import BookingsPage from "./pages/BookingsPage";
 import BookingPage from "./pages/BookingPage";
 
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
-axios.defaults.withCredentials = true;
+// axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+
+axios.defaults.baseURL= "http://localhost:4000/api";
+// axios.defaults.withCredentials = true;
 
 function App() {
   return (
